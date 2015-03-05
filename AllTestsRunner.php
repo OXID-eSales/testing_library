98175d62f91d63e12297234108816eda453de3d0
shiftas <Shiftass@gmail.com>
2015-03-05 18:37:50 +0200

    Set correct header for all files

--- AllTestsRunner.php
+++ AllTestsRunner.php
@@ -1,19 +1,28 @@
 <?php
 /**
- * This file contains the script required to run all PE edition unit tests in unit dir on Cruise Control.
- * This file is supposed to be executed over PHPUnit framework
- * It is called something like this:
- * phpunit <Test dir>_AllTests
+ * This file is part of OXID eSales Testing Library.
  *
- * @link          http://www.oxid-esales.com
- * @copyright (c) OXID eSales AG 2003-#OXID_VERSION_YEAR#
- * @version       SVN: $Id: $
+ * OXID eSales Testing Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales Testing Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales Testing Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2014
  */
 
 echo "=========\nrunning php version " . phpversion() . "\n\n============\n";
 
 /**
- * PHPUnit_Framework_TestCase implementation for adding and testing all unit tests from unit dir
+ * This class is used as a base class to run all CE|PE|EE edition tests.
  */
 class AllTestsRunner extends PHPUnit_Framework_TestCase
 {
