5bb9962a4a245694a613eb2854f47f7fd39f9d65
shiftas <Shiftass@gmail.com>
2015-05-27 15:11:52 +0300

    Rename method and change its visibility
    Rename _executeCommand to executeCommand and change visibility to protected.

--- library/oxFileCopier.php
+++ library/oxFileCopier.php
@@ -48,7 +48,7 @@ class oxFileCopier
                 $command .= " && chmod 777 " . escapeshellarg($target);
             }
         }
-        $this->_executeCommand($command);
+        $this->executeCommand($command);
     }
 
     /**
@@ -71,7 +71,7 @@ class oxFileCopier
      * @param string $directory       Path to directory.
      * @param bool   $removeBaseDir Whether to delete base directory.
      */
-    private function deleteTree($directory, $removeBaseDir = false)
+    protected function deleteTree($directory, $removeBaseDir = false)
     {
         $files = array_diff(scandir($directory), array('.', '..'));
         foreach ($files as $file) {
@@ -92,7 +92,7 @@ class oxFileCopier
      *
      * @return string Output of command.
      */
-    private function _executeCommand($command)
+    protected function executeCommand($command)
     {
         $result = @exec($command, $output, $code);
         $output = implode("\n", $output);
