a8ca2976bef703b73fb184c321f945ff2d00167d
shiftas <Shiftass@gmail.com>
2015-08-31 15:36:55 +0300

    Check if ADONewConnection is already loaded
    Allow overwriting of sShopDir only when oxConfig class is loaded in getShopBasePath, otherwise it can cause troubles with pure unit tests.

--- library/bootstrap/bootstrap_unit.php
+++ library/bootstrap/bootstrap_unit.php
@@ -73,7 +73,7 @@ function getTestsBasePath()
 function getShopBasePath()
 {
     $shopDirectory = OX_BASE_PATH;
-    if (class_exists('oxConfig', false) && class_exists('oxDb', false)) {
+    if (class_exists('oxConfig', false) && class_exists('oxDb', false) && class_exists('ADONewConnection', false)) {
         $configShopDir = oxRegistry::getConfig()->getConfigParam('sShopDir');
         $shopDirectory = $configShopDir ? $configShopDir : $shopDirectory;
     }
