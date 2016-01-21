8cb9a217c6b81ee5c49bdb533855ddb9c5c7f66d
Mantas Vaitkūnas <mantas.vaitkunas@oxid-esales.com>
2016-01-21 17:10:56 +0200

    Add test SQL files selection by edition

---
+++ library/TestSqlPathProvider.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * This file is part of OXID eSales Testing Library.
+ *
+ * OXID eSales Testing Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales Testing Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales Testing Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2016
+ */
+
+namespace OxidEsales\TestingLibrary;
+
+use OxidEsales\Eshop\Core\Edition\EditionRootPathProvider;
+use OxidEsales\Eshop\Core\Edition\EditionSelector;
+
+/**
+ * Class responsible for providing path to testData directory.
+ */
+class TestSqlPathProvider
+{
+    const TEST_SQL_DIRECTORY = 'testSql';
+
+    const TESTS_DIRECTORY = 'tests';
+
+    const ACCEPTANCE_DIRECTORY = 'Acceptance';
+
+    /**
+     * @var EditionSelector
+     */
+    private $editionSelector;
+
+    /**
+     * @var string
+     */
+    private $shopPath = '';
+
+    /**
+     * @param EditionSelector $editionSelector
+     * @param string $shopPath
+     */
+    public function __construct($editionSelector, $shopPath)
+    {
+        $this->editionSelector = $editionSelector;
+        $this->shopPath = $shopPath;
+    }
+
+    /**
+     * Method returns path to test data according edition.
+     *
+     * @param string $testSuitePath
+     *
+     * @return string
+     */
+    public function getDataPathBySuitePath($testSuitePath)
+    {
+        $pathToTestData = $testSuitePath;
+        if ($this->getEditionSelector()->isEnterprise()) {
+            $pathToTestData = $this->updatePathToTestSql($testSuitePath);
+        }
+
+        return $pathToTestData . '/' . static::TEST_SQL_DIRECTORY;
+    }
+
+    /**
+     * @return EditionSelector
+     */
+    protected function getEditionSelector()
+    {
+        return $this->editionSelector;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getShopPath()
+    {
+        return $this->shopPath;
+    }
+
+    /**
+     * Updates provided path for enterprise edition.
+     *
+     * @param string $pathToTestSql
+     *
+     * @return string
+     */
+    protected function updatePathToTestSql($pathToTestSql)
+    {
+        $pathParts = explode(static::TESTS_DIRECTORY . '/' . static::ACCEPTANCE_DIRECTORY, $pathToTestSql);
+        if (count($pathParts) > 1) {
+            $testDirectoryName = $pathParts[count($pathParts) - 1];
+            $pathToEditionTestDirectory = $this->getShopPath()
+                . '/' . EditionRootPathProvider::EDITIONS_DIRECTORY
+                . '/' . EditionRootPathProvider::ENTERPRISE_DIRECTORY
+                . '/' . ucfirst(static::TESTS_DIRECTORY)
+                . '/' . ucfirst(static::ACCEPTANCE_DIRECTORY)
+                . '/' . $testDirectoryName;
+
+            $pathToTestSql = str_replace('//', '/', $pathToEditionTestDirectory);
+        }
+
+        return $pathToTestSql;
+    }
+}
---
+++ tests/unit/TestSqlPathProviderTest.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * This file is part of OXID eSales Testing Library.
+ *
+ * OXID eSales Testing Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales Testing Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales Testing Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2016
+ */
+
+use OxidEsales\Eshop\Core\Edition\EditionSelector;
+use OxidEsales\TestingLibrary\TestSqlPathProvider;
+
+class TestSqlPathProviderTest extends PHPUnit_Framework_TestCase
+{
+    public function providerChecksForCorrectPath()
+    {
+        return [
+            [
+                '/var/www/oxideshop/tests/Acceptance/Admin',
+                EditionSelector::ENTERPRISE,
+                '/var/www/oxideshop/source/Edition/Enterprise/Tests/Acceptance/Admin/testSql'
+            ],
+            [
+                '/var/www/oxideshop/source/Edition/Enterprise/Tests/Acceptance/Admin',
+                EditionSelector::ENTERPRISE,
+                '/var/www/oxideshop/source/Edition/Enterprise/Tests/Acceptance/Admin/testSql'
+            ],
+            [
+                '/var/www/oxideshop/tests/Acceptance/Admin',
+                EditionSelector::COMMUNITY,
+                '/var/www/oxideshop/tests/Acceptance/Admin/testSql'
+            ],
+            [
+                '/var/www/oxideshop/tests/Acceptance/Admin',
+                EditionSelector::PROFESSIONAL,
+                '/var/www/oxideshop/tests/Acceptance/Admin/testSql'
+            ],
+        ];
+    }
+
+    /**
+     * @param string $testSuitePath
+     * @param string $edition
+     * @param string $resultPath
+     *
+     * @dataProvider providerChecksForCorrectPath
+     */
+    public function testChecksForCorrectPath($testSuitePath, $edition, $resultPath)
+    {
+        $shopPath = '/var/www/oxideshop/source';
+        $editionSelector = new EditionSelector($edition);
+        $testDataPathProvider = new TestSqlPathProvider($editionSelector, $shopPath);
+
+        $this->assertSame($resultPath, $testDataPathProvider->getDataPathBySuitePath($testSuitePath));
+    }
+}
