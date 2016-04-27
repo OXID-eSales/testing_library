92b8ae543aac431ab29ce6ea5eccc73b255c20cc
shiftas <Shiftass@gmail.com>
2016-04-27 16:02:05 +0300

    Improve bootstrap for running tests
    Will autoload shop classes for tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,4 +2,7 @@
 
 define('ROOT_DIRECTORY', __DIR__ . '/../');
 
-require_once ROOT_DIRECTORY . 'vendor/autoload.php';
+require_once __DIR__ .'/../base.php';
+$testConfig = new OxidEsales\TestingLibrary\TestConfig();
+
+require_once $testConfig->getShopPath() . '/vendor/autoload.php';
