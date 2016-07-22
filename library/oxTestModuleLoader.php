976b976f43ed5bd342c03b8321e5039475b49001
vilmal <vilma.liorensaityte@gmail.com>
2016-07-22 13:45:09 +0200

    Add deprecations to refactored methods.

--- library/oxTestModuleLoader.php
+++ library/oxTestModuleLoader.php
@@ -24,7 +24,9 @@
  */
 class oxTestModuleLoader
 {
-    /** @var bool Whether to use original chains. */
+    /**
+     * @var bool Whether to use original chains.
+     */
     protected static $original = false;
 
     /**
@@ -40,12 +42,37 @@ class oxTestModuleLoader
     /**
      * Tries to initiate the module classes and includes required files from metadata
      *
+     * @deprecated since v1.0.2 (2016-07-22). Please use the method oxTestModuleLoader::activateModules();
+     *
      * @param array $modulesToActivate Array of modules to load.
      */
     public function loadModules($modulesToActivate)
     {
-        $this->_checkIfModuleToAppendToChain();
+        $this->activateModules($modulesToActivate);
+    }
 
+    /**
+     * Loads the module from metadata file
+     * If no metadata found and the module chain is empty, then does nothing.
+     *
+     * On first load the data is saved and on consecutive calls the saved data is used
+     *
+     * @deprecated since v1.0.2 (2016-07-22). Will be removed, for modules activation please use activateModules();
+     */
+    public function setModuleInformation()
+    {
+    }
+
+    /**
+     * Loads modules and activates them.
+     *
+     * @param array $modulesToActivate Array of modules to load.
+     */
+    public function activateModules($modulesToActivate)
+    {
+        $this->clearModuleChain();
+
+        // First load all needed config options before the module will be installed.
         $this->prepareModulesForActivation();
         foreach ($modulesToActivate as $modulePath) {
             $this->installModule($modulePath);
@@ -100,7 +127,7 @@ class oxTestModuleLoader
     /**
      * Checks if extended files have to be added to "original" module chain or to empty chain.
      */
-    private function _checkIfModuleToAppendToChain()
+    private function clearModuleChain()
     {
         if (!self::$original) {
             oxRegistry::getConfig()->setConfigParam("aModules", '');
