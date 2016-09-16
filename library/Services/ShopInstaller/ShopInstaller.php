bf90730d9b16c044684f9c2d0734f3ac6b027b19
vilmal <vilma.liorensaityte@gmail.com>
2016-09-16 15:56:06 +0200

    Set configuration parameters during shop installation. (cherry picked from commit 1a0a420)

--- library/Services/ShopInstaller/ShopInstaller.php
+++ library/Services/ShopInstaller/ShopInstaller.php
@@ -97,6 +97,8 @@ class ShopInstaller implements ShopServiceInterface
             $this->convertToInternational();
         }
 
+        $this->setConfigurationParameters();
+
         if ($this->getDbHandler()->getCharsetMode() == 'utf8') {
             $this->convertToUtf();
         }
