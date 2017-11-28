0af91309c8ff574c0acf5f5e5200b69adce57ded
sebbbbauer <sebastian.bauer@oxid-esales.com>
2017-11-28 10:30:22 +0100

    Adjust license header

--- library/MC_Metrics.php
+++ library/MC_Metrics.php
@@ -1,14 +1,7 @@
 <?php
 /**
- * This Software is the property of OXID eSales and is protected
- * by copyright law - it is NOT Freeware.
- *
- * Any unauthorized use of this software without a valid license key
- * is a violation of the license agreement and will be prosecuted by
- * civil and criminal law.
- *
- * @link http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2013
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
  */
 
 ini_set("display_errors", false);
